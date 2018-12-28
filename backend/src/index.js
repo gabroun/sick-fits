3adbad8a2aa61edc89a1b8604705044291ff26ef
Malik Elgabroun <malikgabroun@yahoo.co.uk>
2018-12-28 18:18:50 +0000

    creating yoga server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,19 @@
-// let's go!
+require('dotenv').config({path: 'variables.env'});
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// Todo use express middleware to handle cookies (JWT)
+// Todo use express middleware to populate current user
+
+server.start({
+     //we only want this endpoint to be visited by approved urls as you dont want anyone to access your website from a different website, you only want our website to hit that
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    }
+}, deets => {
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
+}
+);
