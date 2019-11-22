d8584f2c3939b327cac5afc51c4ab92ca9835769
Malik Elgabroun <malikgabroun@yahoo.co.uk>
2019-11-22 23:02:28 +0000

    delete item mutation/resolver and delete item component

--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -6,6 +6,7 @@ import formatMoney from '../lib/formatMoney';
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
 import Title from './styles/Title';
+import DeleteItem from './DeleteItem'
 
 class Item extends React.Component {
   static propTypes = {
@@ -39,7 +40,7 @@ class Item extends React.Component {
             <a>Edit ✏️</a>
           </Link>
           <button>Add To Cart</button>
-          <button>Delete</button>
+          <DeleteItem id={item.id}>Delete Item</DeleteItem>
         </div>
       </ItemStyles>
     );
